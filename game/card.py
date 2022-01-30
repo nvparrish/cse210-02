cf569dd9a5cd7b6f0a2d035ef0ad2eb4809d96fe
CALEB GENTRY <gen14001@byui.edu>
2022-01-29 21:38:53 -0600

    Deck and Player class created

--- game/card.py
+++ game/card.py
@@ -1,1 +1,9 @@
 """THIS IS WHERE WE CAN CREATE THE DECK OF RANDOM CARDS FOR THE GAME"""
+import random
+
+class Deck:
+    def __init__(self):
+        self.numbers = random.randint(1,13)
+
+    def getnumbers(self):
+        return self.numbers
--- game/dealer-player.py
+++ game/dealer-player.py
@@ -1,2 +1,32 @@
 """THIS IS WHERE THE "DEALER" WILL ASK QUESTIONS AND WILL REVEAL
 THE CARDS TO THE PLAYER"""
+
+from game.card import Deck
+
+class Players:
+    def __init__(self):
+        self.first = Deck()
+        self.is_playing = True
+        self.score = 300
+
+    def begin(self):
+        while self.is_playing:
+            pass
+
+    def player_input(self):
+        cardnumber=self.first.value
+        print("The card is: %d" %cardnumber)
+
+    dealer = ""
+    while dealer != "h" or dealer != "l":
+        dealer = input("Higher or Lower? [h/l]: ")
+        previous_card = self.first
+        new_card = Deck()
+        if(dealer == "h"):
+            self.high(previous_card,new_card)
+            break
+        elif(dealer == "l"):
+            self.low(previous_card,new_card)
+            break
+        else:
+            print("Choose h or l only.")
