e37a92d9e61d1964a5cd5cdf3ac9d76d75d6ec32
CALEB GENTRY <gen14001@byui.edu>
2022-02-03 20:35:35 -0600

    main connected to dealer. Bug fix on dealer

---
+++ game/dealer.py
@@ -0,0 +1,74 @@
+"""THIS IS WHERE THE "DEALER" WILL ASK QUESTIONS AND WILL REVEAL
+THE CARDS TO THE PLAYER"""
+
+from game.card import Deck
+
+class Players:
+    def __init__(self):
+        self.first = Deck()
+        self.is_playing = True
+        self.score = 300
+
+    def begin(self):
+        while self.is_playing:
+            self.player_input()
+            self.player_output()
+
+    def player_input(self):
+        cardnumber=self.first.numbers
+        print("The card is: %d" %cardnumber)
+
+        dealer = ""
+        while dealer != "h" or dealer != "l":
+            dealer = input("Higher or Lower? [h/l]: ")
+            previous_card = self.first
+            new_card = Deck()
+            if(dealer == "h"):
+                self.high(previous_card,new_card)
+                break
+            elif(dealer == "l"):
+                self.low(previous_card,new_card)
+                break
+            else:
+                print("Choose h or l only.")
+
+    def updates(self, guess, new_card):
+        self.score += guess 
+        if (self.score <= 0):
+            self.is_playing = False
+        self.first.numbers=new_card
+
+    def player_output(self):
+        print(f"Your score is: {self.score}")
+        print(f"The next card was: {self.first.numbers}")
+
+        if(self.score <=0):
+            self.is_playing = False
+            return
+
+        abc=""
+        while abc != "n" or "y":
+            abc = input("Play again? [y/n]: ")
+            if(abc == "n"):
+                self.is_playing = False
+                break
+            elif(abc == "y"):
+                print(" ")
+                break
+            else:
+                print("Choose y or n only.")
+
+    def high(self, card1, card2):
+        if card2.numbers > card1.numbers:
+            self.updates(100, card2.numbers)
+        elif card2.numbers < card1.numbers:
+            self.updates(-75,card2.numbers)
+
+    def low(self, card1, card2):
+        if card2.numbers < card1.numbers:
+            self.updates(100,card2.numbers)
+        elif card2.numbers > card1.numbers:
+            self.updates(-75,card2.numbers)
+        else:
+            print(" ")
+            return
--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 """THIS WILL BE OUR MAIN FILE WHERE WE PLAY THE GAME"""
+from game.dealer import Players
+
+dealer = Players()
+dealer.begin()
